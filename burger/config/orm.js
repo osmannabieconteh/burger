cfbe3007ba486a172bc935705bfa5eab76257349
osman conteh <osmannabieconteh@gmail.com>
2019-11-10 16:44:33 -0500

    newcharges

---
+++ burger/config/orm.js
@@ -0,0 +1,22 @@
+var connection = require("connection.js");
+var orm = {
+
+
+selectAll: function() {
+      if (this.raining === true) {
+        console.log("select from");
+      },
+
+      
+insertOne function() {
+    if (this.raining === true) {
+      console.log("input your item");
+    }
+
+    
+updateOne function() {
+    if (this.raining === true) {
+      console.log("item updated");
+    }
+  }
+    module.exports = orm;
---
+++ burger/db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burg id_pet, burger_name, devoured)
+VALUES ("Ahmed", TRUE, "Rockington", 100);
+
+INSERT INTO burgers ( id_pet, burger_name, devoured)
+VALUES ("Ahmed", TRUE, "Rockington", 100);
+
+INSERT INTO burgers ( id_pet, burger_name, devoured)
+VALUES ("Jacob", TRUE, "Misty", 10);
